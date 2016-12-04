8001d9d292d1cb27ba57c2f3261611ece7c77326
Matt Long <matt87611@gmail.com>
2016-12-04 08:03:09 +0000

    06b

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <p>Catch of the day</p>
+          <Header/>
         </div>
+        <Order/>
+        <Inventory/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
 
-import StorePicker from './components/StorePicker.js'
-import App from './components/App.js'
+//import StorePicker from './components/StorePicker';
+import App from './components/App';
 
 render(<App/>, document.querySelector('#main'));
