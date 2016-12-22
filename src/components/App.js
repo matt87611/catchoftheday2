07d433d3bb8fec1abf8548381fa02ca84547d35f
Matt Long <matt87611@gmail.com>
2016-12-22 19:52:00 +0000

    21b

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
 
     this.state = {
       fishes: {},
@@ -91,16 +92,26 @@ class App extends React.Component {
     this.setState({order: order});
   }
 
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({order});
+  }
+
   render() {
     return (
-      <div className="catch-of-the-day">``
+      <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
             {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params}
+          removeFromOrder={this.removeFromOrder}/>
         <Inventory
           addFish={this.addFish}
           loadSamples={this.loadSamples}
