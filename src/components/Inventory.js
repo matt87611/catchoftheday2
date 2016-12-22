99a61e3bdb60fcd754239e83d9fe69fec9c29036
Matt Long <matt87611@gmail.com>
2016-12-22 19:27:42 +0000

    20b

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -24,13 +24,13 @@ class Inventory extends React.Component {
     return (
       <div className="fish-edit" key={key}>
         <input type="text" name="name" value={fish.name} placeholder="Fish Name" onChange={(e) => this.handleChange(e, key)}/>
-        <input type="text" name="price" placeholder="Fish Price" onChange={(e) => this.handleChange(e, key)}/>
-        <select type="text" name="status" placeholder="Fish Status" onChange={(e) => this.handleChange(e, key)}>
+        <input type="text" name="price" value={fish.price} placeholder="Fish Price" onChange={(e) => this.handleChange(e, key)}/>
+        <select type="text" name="status" value={fish.status} placeholder="Fish Status" onChange={(e) => this.handleChange(e, key)}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea type="text" name="desc" placeholder="Fish Desc" onChange={(e) => this.handleChange(e, key)}></textarea>
-        <input type="text" name="image" placeholder="Fish Image" onChange={(e) => this.handleChange(e, key)}/>
+        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={(e) => this.handleChange(e, key)}/>
       </div>
     )
   }
